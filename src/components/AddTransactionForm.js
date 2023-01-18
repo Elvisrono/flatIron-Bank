4ae1ee8bc8656be1647a8f571d9dac7bf480d0b0
¨kericho¨ <lvisrono90@gmail.com>
2023-01-18 12:49:20 +0300

    Completed

---
+++ src/components/AddTransactionForm.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+function AddTransactionForm({fetchFunction}) {
+  const [submitted, setSubmitted] = useState(null);
+  // const [addedData, setAddedData] = useState(null);
+
+  useEffect(() => {
+   fetchFunction(submitted)
+
+  })
+   //[submitted])
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const date = e.target.date.value;
+    const description = e.target.description.value;
+    const category = e.target.category.value;
+    const amount = e.target.amount.value;
+
+    const formData = {
+      date: date,
+      description: description,
+      category: category,
+      amount: amount
+    }
+    // set submitted data
+    setSubmitted(formData);
+    e.target.reset();
+
+  }
+
+  return (
+    <div className="ui segment">
+      <form className="formbox" onSubmit={handleSubmit}>
+        <div className="form">
+          <input type="date" name="date" />
+          <input type="text" name="description" placeholder="Description" />
+          <input type="text" name="category" placeholder="Category" />
+          <input type="number" name="amount" placeholder="Amount" />
+          <br></br>
+        </div>
+        <br></br>
+        <br></br>
+        <button className="btn btn-outline-info ms-1" type="submit">
+          Add Transaction
+        </button>
+      </form>
+
+    </div>
+  );
+}
+
+export default AddTransactionForm;
---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+
+function App() {
+  return (
+
+    
+    <div className="ui container">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand bg-blue" href="The Bank Of Flatiron"> </a>
+          
+
+        </div>
+       
+      </nav>
+     
+<h1 className="display">The Bank Of Flatiron</h1>
+
+      <AccountContainer />
+    </div>
+    
+  );
+}
+
+export default App;
